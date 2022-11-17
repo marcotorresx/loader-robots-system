5bb0622af8cec3c9bfb54397f078200b6305c77a
Marco Torres <marcotorres258@outlook.com>
2022-11-17 09:39:21 -0600

    Server check

--- robots_sim_2d/server.py
+++ robots_sim_2d/server.py
@@ -3,9 +3,6 @@ from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.UserParam import UserSettableParameter
 
-
-PERCENTAGES = {"Cleaned": "#00AA00", "Dirty": "#000000"}
-
 def agent_portrayal(agent):
     if agent is None: return
     
@@ -18,27 +15,30 @@ def agent_portrayal(agent):
     if (isinstance(agent, ObstacleAgent)):
         portrayal["Color"] = "grey"
         portrayal["Layer"] = 1
-        portrayal["r"] = 0.2
+        portrayal["r"] = 0.3
 
     if (isinstance(agent, BoxAgent)):
-        portrayal["Color"] = "blue"
+        portrayal["Color"] = "brown"
         portrayal["Layer"] = 1
         portrayal["r"] = 0.2 
 
     if (isinstance(agent, DestinyAgent)):
         portrayal["Color"] = "green"
         portrayal["Layer"] = 1
-        portrayal["r"] = 0.2 
+        portrayal["r"] = 0.7 
 
     return portrayal
 
 grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
 
-model_params = {"N": UserSettableParameter(
-                "slider", "Number of roombas", 1, 1, 10, 1), "width":10, "height":10, 
-                "timer": UserSettableParameter("slider", "Timer", 10, 10, 100, 5)}
+model_params = {
+    "N": UserSettableParameter("slider", "Number of roombas", 1, 1, 10, 1), 
+    "width": 10, 
+    "height": 10, 
+    "timer": UserSettableParameter("slider", "Timer", 10, 10, 100, 5)
+}
 
-server = ModularServer(RandomModel, [grid], "Roomba Simulation", model_params)
+server = ModularServer(RandomModel, [grid], "Loader Robots", model_params)
                        
 server.port = 8521 # The default
 server.launch()
