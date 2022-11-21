09695d41508fd466ab266df5123ed43f218a9e14
Marco Torres <marcotorres258@outlook.com>
2022-11-21 12:48:02 -0600

    Comment server

--- Server/agents/BoxAgent.py
+++ Server/agents/BoxAgent.py
@@ -7,7 +7,7 @@ class BoxAgent(Agent):
 
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.isTaken = False
+        self.is_taken = False
         self.order = 0
 
     def step(self):
