f01f71253e7e73686f3dba0904292536b6d5c230
Marco Torres <marcotorres258@outlook.com>
2022-11-21 20:42:09 -0600

    Comment script

--- LoaderRobotsUnity/Assets/Scripts/AgentController.cs
+++ LoaderRobotsUnity/Assets/Scripts/AgentController.cs
@@ -57,7 +57,7 @@ public class AgentController : MonoBehaviour
     public int NAgents, width, height, boxes;
     public float timeToUpdate = 5.0f;
     private float timer, dt;
-    bool updated = false, started = false, started2 = false;
+    bool updated = false, agents_started = false, boxs_started = false;
     public GameObject agentPrefab, obstaclePrefab, destinyPrefab, boxPrefab, floor;
 
     void Start()
@@ -68,29 +68,32 @@ public class AgentController : MonoBehaviour
         boxData = new AgentsData();
         destinyData = new AgentsData();
 
+        // Agent dictionary
+        agents = new Dictionary<string, GameObject>();
+        
+        // Positions
         prevPositions = new Dictionary<string, Vector3>();
         currPositions = new Dictionary<string, Vector3>();
 
-        agents = new Dictionary<string, GameObject>();
-
         floor.transform.localScale = new Vector3((float) width/10, 1, (float) height/10);
         floor.transform.localPosition = new Vector3((float) width/2-0.5f, 0, (float) height/2-0.5f);
-        
         timer = timeToUpdate;
 
+        // Configurate model
         StartCoroutine(SendConfiguration());
     }
 
 
     private void Update() 
     {
-        if(timer < 0)
+        // When the agents arrived to their new positions
+        if (timer < 0)
         {
             timer = timeToUpdate;
             updated = false;
             StartCoroutine(UpdateSimulation());
         }
-
+        // Move objects to their new positions
         if (updated)
         {
             timer -= Time.deltaTime;
@@ -105,7 +108,10 @@ public class AgentController : MonoBehaviour
                 Vector3 direction = currentPosition - interpolated;
 
                 agents[agent.Key].transform.localPosition = interpolated;
-                if (direction != Vector3.zero) agents[agent.Key].transform.rotation = Quaternion.LookRotation(direction);
+                
+                // Only rotate agents (with ids from 1000 to 1999), boxes rotation will remain the same
+                if (int.Parse(agent.Key) < 2000)
+                    if (direction != Vector3.zero) agents[agent.Key].transform.rotation = Quaternion.LookRotation(direction);
             }
         }
     }
@@ -120,6 +126,7 @@ public class AgentController : MonoBehaviour
             Debug.Log(www.error);
         else 
         {
+            // Get new positions
             StartCoroutine(GetAgentsData());
             StartCoroutine(GetBoxData());
             StartCoroutine(GetDestinyData());
@@ -131,6 +138,7 @@ public class AgentController : MonoBehaviour
     {
         WWWForm form = new WWWForm();
 
+        // Model params
         form.AddField("NAgents", NAgents.ToString());
         form.AddField("width", width.ToString());
         form.AddField("height", height.ToString());
@@ -149,6 +157,8 @@ public class AgentController : MonoBehaviour
         {
             Debug.Log("Configuration completed!");
             Debug.Log("Getting agents positions...");
+
+            // Get agents positions
             StartCoroutine(GetAgentsData());
             StartCoroutine(GetBoxData());
             StartCoroutine(GetObstacleData());
@@ -159,6 +169,7 @@ public class AgentController : MonoBehaviour
 
     IEnumerator GetAgentsData() 
     {
+        // Get agents positions
         UnityWebRequest www = UnityWebRequest.Get(serverUrl + getAgentsEndpoint);
         yield return www.SendWebRequest();
  
@@ -167,32 +178,32 @@ public class AgentController : MonoBehaviour
         else 
         {
             agentsData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
-            Debug.Log(www.downloadHandler.text);
-            foreach(AgentData agent in agentsData.positions)
+            foreach (AgentData agent in agentsData.positions)
             {
                 Vector3 newAgentPosition = new Vector3(agent.x, agent.y, agent.z);
-                    if(!started)
-                    {
-                        prevPositions[agent.id] = newAgentPosition;
-                        agents[agent.id] = Instantiate(agentPrefab, newAgentPosition, Quaternion.identity);
-                    }
-                    else
-                    {
-                        Vector3 currentPosition = new Vector3();
-                        if(currPositions.TryGetValue(agent.id, out currentPosition))
-                            prevPositions[agent.id] = currentPosition;
-                        currPositions[agent.id] = newAgentPosition;
-                    }
+                // If its the first step
+                if (!agents_started)
+                {
+                    prevPositions[agent.id] = newAgentPosition; // New positions will be previous
+                    agents[agent.id] = Instantiate(agentPrefab, newAgentPosition, Quaternion.identity); // Set game object
+                }
+                else
+                {
+                    Vector3 currentPosition = new Vector3();
+                    if (currPositions.TryGetValue(agent.id, out currentPosition))
+                        prevPositions[agent.id] = currentPosition; // Current position will be previous
+                    currPositions[agent.id] = newAgentPosition; // New position will be current
+                }
             }
-
             updated = true;
-            if (!started) started = true;
+            if (!agents_started) agents_started = true;
         }
     }
 
 
     IEnumerator GetBoxData() 
     {
+        // Get boxs positions
         UnityWebRequest www = UnityWebRequest.Get(serverUrl + getBoxesEndpoint);
         yield return www.SendWebRequest();
  
@@ -201,36 +212,32 @@ public class AgentController : MonoBehaviour
         else 
         {
             boxData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
-
-            Debug.Log(www.downloadHandler.text);
-
             foreach(AgentData box in boxData.positions)
             {
-
                 Vector3 newBoxPosition = new Vector3(box.x, box.y, box.z);
-
-                    if(!started2)
+                    // If its the first step
+                    if (!boxs_started)
                     {
-                        prevPositions[box.id] = newBoxPosition;
-                        agents[box.id] = Instantiate(boxPrefab, newBoxPosition, Quaternion.identity);
+                        prevPositions[box.id] = newBoxPosition; // New positions will be previous
+                        agents[box.id] = Instantiate(boxPrefab, newBoxPosition, Quaternion.identity); // Set game object
                     }
                     else
                     {
                         Vector3 currentPosition = new Vector3();
-                        if(currPositions.TryGetValue(box.id, out currentPosition))
-                            prevPositions[box.id] = currentPosition;
-                        currPositions[box.id] = newBoxPosition;
+                        if (currPositions.TryGetValue(box.id, out currentPosition))
+                            prevPositions[box.id] = currentPosition; // Current position will be previous
+                        currPositions[box.id] = newBoxPosition; // New position will be current
                     }
             }
-
             updated = true;
-            if (!started2) started2 = true;
+            if (!boxs_started) boxs_started = true;
         }
     }
 
 
     IEnumerator GetDestinyData() 
     {
+        // Get destinations positions
         UnityWebRequest www = UnityWebRequest.Get(serverUrl + getDestinyEndpoint);
         yield return www.SendWebRequest();
  
@@ -239,9 +246,6 @@ public class AgentController : MonoBehaviour
         else 
         {
             destinyData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
-
-            Debug.Log(www.downloadHandler.text);
-
             foreach (AgentData destiny in destinyData.positions)
             {
                 Instantiate(destinyPrefab, new Vector3(destiny.x, destiny.y, destiny.z), Quaternion.identity);
@@ -252,6 +256,7 @@ public class AgentController : MonoBehaviour
 
     IEnumerator GetObstacleData() 
     {
+        // Get obtacles positions
         UnityWebRequest www = UnityWebRequest.Get(serverUrl + getObstaclesEndpoint);
         yield return www.SendWebRequest();
  
@@ -260,13 +265,8 @@ public class AgentController : MonoBehaviour
         else 
         {
             obstacleData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
-                Debug.Log(www.downloadHandler.text);
-
-
             foreach(AgentData obstacle in obstacleData.positions)
             {
-                Debug.Log(obstacle);
-
                 Instantiate(obstaclePrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
             }
         }
